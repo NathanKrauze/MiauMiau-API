938bfe2fc0350750976760d39e93c5a77fb69a77
NathanKrauze <krauzenathan@gmail.com>
2023-10-03 10:58:12 -0300

    feat: simple code for deploy

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+    res.send('ok');
+})
+
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Server running on port ${port}`)
+});
